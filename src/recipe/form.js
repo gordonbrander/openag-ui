0ea17ef81b05bcf7f081e719e3e2300584f94092
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-04-20 11:34:41 -0400

    Add submit button
    This is a bit kludgy. Need to create a general module for text input.

--- src/recipe/form.js
+++ src/recipe/form.js
@@ -5,15 +5,21 @@ import {html, forward, Effects} from 'reflex';
 import * as Unknown from '../common/unknown';
 import * as Recipes from '../recipes';
 
+// Will not create a property field.
+const nil = void(0);
+
 export const init = () => [
   {
-    isOpen: true
+    isOpen: false
   },
   Effects.none
 ];
 
 // Submitting the form
-const Submit = {type: 'Submit'};
+const Create = operations => ({
+  type: 'Create',
+  operations
+});
 
 export const update = (model, action) =>
   // @TODO
@@ -24,19 +30,34 @@ export const update = (model, action) =>
 export const view = (model, address) =>
   html.dialog({
     className: 'rform-main',
-    open: (model.isOpen ? 'open' : null)
+    open: (model.isOpen ? 'open' : nil)
   }, [
-    html.textarea({
-      className: 'rform-textarea'
-    }),
-    html.footer({
-      className: 'rform-footer',
+    html.form({
+      className: 'rform-form',
+      onSubmit: (event) => {
+        // @TODO create a proper input module instead of kludging this in a
+        // brittle way. We want to be able to send an Effect that will
+        // focus, unfocus. We also want to read value changes from `onInput`.
+        // See https://github.com/browserhtml/browserhtml/blob/master/src/common/ref.js
+        // https://gist.github.com/Gozala/2b6a301846b151aafe807104304dbd06#file-focus-js
+        event.preventDefault();
+        const el = document.querySelector('#rform-textarea');
+        address(Create(el.value));
+      }
     }, [
-      html.button({
-        className: 'btn-primary',
-        onClick: () => address(Submit)
+      html.textarea({
+        className: 'rform-textarea',
+        id: 'rform-textarea',
+      }),
+      html.footer({
+        className: 'rform-footer',
       }, [
-        'Create'
+        html.button({
+          className: 'btn-primary',
+          type: 'submit',
+        }, [
+          'Create'
+        ])
       ])
     ])
   ]);
