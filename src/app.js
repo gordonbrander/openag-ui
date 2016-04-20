bd9a0926d3437406dac341db3a74489ce5ef35df
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-04-20 14:02:12 -0400

    Rendering basic sensor view from actions

--- src/app.js
+++ src/app.js
@@ -1,28 +1,62 @@
 import {html, forward, Effects} from 'reflex';
-import {merge, tag} from './common/prelude';
+import {merge, tagged, tag} from './common/prelude';
 import * as Unknown from './common/unknown';
 import {cursor} from './common/cursor';
 import * as Recipes from './recipes';
 import * as RecipeForm from './recipe/form';
 
+// @TODO we should move this under a "Dashboard" module and have all the
+// sensor reading modules plug into this parent module.
+import * as AirTemperature from './sensors/air-temperature';
+
+// Actions
+
+const CreateRecipe = operations => ({
+  type: 'CreateRecipe',
+  operations
+});
+
 // Action tagging functions
 
-const RecipeFormAction = tag('RecipeForm');
+const AirTemperatureAction = tag('AirTemperature');
+const RecipesAction = tag('Recipes');
+
+const RecipeFormAction = action =>
+  action.type === 'Create' ?
+  CreateRecipe(create.operations) :
+  tagged('RecipeForm', action);
+
+// Init and update
 
 export const init = () => {
+  const [airTemperature, airTemperatureFx] = AirTemperature.init({
+    type: 'air_temperature',
+    title: 'Air Temperature',
+    value: null
+  });
   const [recipeForm, recipeFormFx] = RecipeForm.init();
   const [recipes, recipesFx] = Recipes.init([]);
+
   return [
     {
+      airTemperature,
       recipeForm,
       recipes
     },
     Effects.batch([
-      recipeFormFx,
-      recipesFx
+      airTemperatureFx.map(AirTemperatureAction),
+      recipeFormFx.map(RecipeFormAction),
+      recipesFx.map(RecipesAction)
     ])
   ];
 }
+
+const updateAirTemperature = cursor({
+  get: model => model.airTemperature,
+  set: (model, airTemperature) => merge(model, {airTemperature}),
+  update: AirTemperature.update,
+  tag: AirTemperatureAction
+});
 
 const updateRecipeForm = cursor({
   get: model => model.recipeForm,
@@ -32,12 +66,17 @@ const updateRecipeForm = cursor({
 });
 
 export const update = (model, action) =>
+  //action.type === 'CreateRecipe' ?
+  //updateRecipes(model, Recipes.Create(action.operations)) :
+  action.type === 'AirTemperature' ?
+  updateAirTemperature(model, action.source) :
   action.type === 'RecipeForm' ?
-  updateRecipeForm(model, action) :
+  updateRecipeForm(model, action.source) :
   Unknown.update(model, action);
 
 export const view = (model, address) => html.div({
   className: 'app-main'
 }, [
+  AirTemperature.view(model.airTemperature, forward(address, AirTemperatureAction)),
   RecipeForm.view(model.recipeForm, forward(address, RecipeFormAction))
 ]);
