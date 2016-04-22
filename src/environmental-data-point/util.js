989b7ca7916451617ae5caf3b1a1f0e36308e553
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-04-22 15:20:09 -0400

    Add note

--- src/environmental-data-point/util.js
+++ src/environmental-data-point/util.js
@@ -1,4 +1,6 @@
 import * as Lang from './variable-lang';
 
+// @FIXME this is a temporary measure. Eventually we should add real
+// localization for variable names.
 export const readName = (variable) =>
   Lang[variable] ? Lang[variable] : variable;
