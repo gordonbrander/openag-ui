5fc55fe9ec24ca5e902b6d88fe9fa7ab398f5963
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-06-27 14:48:35 -0400

    Extremely basic rendering of Rickshaw graph

--- scripts/environmental-data-point/line-chart.js
+++ scripts/environmental-data-point/line-chart.js
@@ -3,81 +3,80 @@ import {html, forward, Effects} from 'reflex';
 import {merge, tag} from '../common/prelude';
 import * as Ordered from '../common/ordered';
 import * as Unknown from '../common/unknown';
-import Chart from 'chart.js';
+import Rickshaw from 'rickshaw';
 
 // Limit for number of datapoints to keep for dashboard render.
 const MAX = 500;
+const MS_SECOND = 1000;
+const MS_MINUTE = 60 * MS_SECOND;
+const MS_HOUR = MS_MINUTE * 60;
 
 // Define a widget wrapper for Chart.js. This lets us define a block of code
 // that doesn't get patched via virtual dom, which is important since
 // chart.js manages its own lifecycle and event loop.
 // See virtual-dom widgets for more
-// https://github.com/Matt-Esch/virtual-dom/blob/master/docs/widget.md<Paste>
+// https://github.com/Matt-Esch/virtual-dom/blob/master/docs/widget.md
 class LineChartWidget {
   constructor(label, data) {
     this.type = 'Widget';
     this.label = label;
-    this.data = Ordered.toArray(data, compareByX);
+    this.data = data;
   }
 
   init() {
-    const parent = document.createElement('div');
-    parent.style.width = '100%';
-    const canvas = document.createElement('canvas');
-    parent.appendChild(canvas);
+    const element = document.createElement('div');
 
-    const chart = new Chart(canvas, {
-      type: 'line',
-      // Data should be of type [{x, y}]
-      data: {
-        datasets: [
-          {
-            label: this.label,
-            data: this.data
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        // Do not show x/y grid lines
-        scales: {
-          yAxes: [
-            {
-              gridLines: {
-                display: false
-              }
-            }
-          ],
-          xAxes: [{
-            type: 'time',
-            time: {
-              displayFormats: {
-                minute: 'h:mm a'
-              }
-            },
-            gridLines: {
-              display: false
-            },
-            position: 'bottom',
-          }]
-        }
+    const series = [
+      {
+        renderer: 'line',
+        name: this.label,
+        color: '#0052b3',
+        data: toArrayOrderedByX(this.data)
       }
+    ];
+
+    // const series = new Rickshaw.Series.FixedDuration(this.data, null, {
+    //   timeInterval: MS_HOUR,
+    //   maxDataPoints: 100,
+    //   timeBase: new Date().getTime() / 1000
+    // });
+
+    const graph = new Rickshaw.Graph({
+      element,
+      series,
+      width: window.innerWidth,
+      height: 250,
+      // preserve: true,
+      renderer: 'line',
+      interpolation: 'linear',
+      min: 0,
+      max: 50
     });
 
-    this.chart = chart;
+    const detail = new Rickshaw.Graph.HoverDetail({
+      graph: graph
+    });
 
-    return parent;
+    // const slider = new Rickshaw.Graph.RangeSlider.Preview({
+    //   graph: graph,
+    //   element: document.querySelector('#slider')
+    // });
+
+    this.graph = graph;
+
+    return element;
   }
 
   update(widget, el) {
-    this.chart = widget.chart;
-    this.chart.data.datasets[0].data = this.data;
-    this.chart.resize();
-    this.chart.update();
+    this.graph = widget.graph;
+    if (widget.data !== this.data) {
+      this.graph.series[0].data = toArrayOrderedByX(this.data);
+      this.graph.render();
+    }
   }
 
   destroy(el) {
-    this.chart.destroy();
+    this.graph.destroy();
   }
 }
 
@@ -123,14 +122,14 @@ export const update = (model, action) =>
 
 const addMeasured = (model, dataPoint) => [
   merge(model, {
-    measured: Ordered.insert(model.measured, readDataPoint(dataPoint), getX)
+    measured: Ordered.insertNew(model.measured, readDataPoint(dataPoint), getX)
   }),
   Effects.none
 ];
 
 const addDesired = (model, dataPoint) => [
   merge(model, {
-    desired: Ordered.insert(model.desired, readDataPoint(dataPoint), getX)
+    desired: Ordered.insertNew(model.desired, readDataPoint(dataPoint), getX)
   }),
   Effects.none
 ];
@@ -158,12 +157,14 @@ const isDesired = dataPoint => dataPoint.is_desired;
 const isMeasured = dataPoint => !dataPoint.is_desired;
 
 const readDataPoint = ({timestamp, value}) => ({
-  x: new Date(timestamp * 1000),
+  x: Math.floor(timestamp),
   y: Number.parseFloat(value)
 });
 
 // Sort by x (timestamp) desc.
 const compareByX = (a, b) => a.x > b.x ? 1 : -1;
+
+const toArrayOrderedByX = object => Ordered.toArray(object, compareByX);
 
 // View
 
