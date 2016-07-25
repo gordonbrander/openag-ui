826faf4bd6a4e083b0ea9b176a5f196588e14a9e
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-07-25 15:39:00 -0400

    Add refresh button to banner

--- scripts/banner.js
+++ scripts/banner.js
@@ -1,38 +1,41 @@
 /*
 An error banner for notifying the user of edge-case conditions.
 */
+import * as Lang from './common/lang';
 import {html, Effects, forward} from 'reflex';
-import {tag, batch} from './common/prelude';
+import {tag} from './common/prelude';
 import * as ClassName from './common/classname';
 import * as Unknown from './common/unknown';
 
 // Actions
 
-export const ModalAction = tag('Modal');
-
 export const Open = {type: 'Open'};
 export const Close = {type: 'Close'};
 export const Alert = tag('Alert');
+export const AlertWithRefresh = tag('AlertWithRefresh');
 
 // Update, model, init
 
-export const Model = (isOpen, message) => ({
+export const Model = (isOpen, isRefresh, message) => ({
   isOpen,
+  isRefresh,
   message
 });
 
 // Convenient update functions for model
-const open = model => Model(true, model.message);
-const close = model => Model(close, model.message);
+const open = model => Model(true, model.isRefresh, model.message);
+const close = model => Model(false, model.isRefresh, model.message);
 
 export const init = () => [
-  Model(true, 'There seems to be a problem with your computer'),
+  Model(true, true, 'There seems to be a problem with your computer'),
   Effects.none
 ];
 
 export const update = (model, action) =>
   action.type === 'Alert' ?
-  [Model(true, action.source), Effects.none] :
+  [Model(true, false, action.source), Effects.none] :
+  action.type === 'AlertWithRefresh' ?
+  [Model(true, true, action.source), Effects.none] :
   action.type === 'Open' ?
   [open(model), Effects.none] :
   action.type === 'Close' ?
@@ -52,5 +55,18 @@ export const view = (model, address) =>
       className: 'banner--text'
     }, [
       model.message
+    ]),
+    html.a({
+      className: ClassName.create({
+        'banner--refresh': true,
+        'banner--refresh-close': !model.isRefresh
+      }),
+      onClick: event => {
+        event.preventDefault();
+        // Refresh and flip boolean to bypass page cache.
+        document.location.reload(true);
+      }
+    }, [
+      Lang.localize('Refresh')
     ])
   ]);
---
+++ scripts/common/lang.js
@@ -0,0 +1,6 @@
+/*
+@FIXME this file is a placeholder. It has a simple identity function that will
+become a real translation function in future.
+*/
+
+export const localize = text => text;
