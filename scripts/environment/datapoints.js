2f744decf6faedb04c186536a31adfe457fd5f29
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-08-26 17:31:31 -0400

    Search for recipe start and update sidebar
    When fetching backlog

--- scripts/environment/datapoints.js
+++ scripts/environment/datapoints.js
@@ -2,6 +2,12 @@
 // monomorphic. This gives the JavaScript JIT an opportunity to do some
 // optimization, treating instances as structs in the backend.
 
+import {findRight} from '../lang/find';
+
+const RECIPE_START = 'recipe_start';
+const RECIPE_END = 'recipe_end';
+const MARKER = 'marker';
+
 class StringPoint {
   constructor(variable, is_desired, is_manual, timestamp, value) {
     this.variable = variable;
@@ -25,7 +31,40 @@ class NumberPoint {
 export const marker = (timestamp, value) =>
   new StringPoint('marker', false, true, timestamp, value);
 
-export const isMarker = dataPoint => dataPoint.variable === 'marker';
+export const isMarker = dataPoint => dataPoint.variable === MARKER;
+export const isRecipeStart = dataPoint => dataPoint.variable === RECIPE_START;
+export const isRecipeEnd = dataPoint => dataPoint.variable === RECIPE_END;
+
+export const isRecipeRunning = (recipeStart, recipeEnd) => {
+  // If we have both a recipe start and a recipe end, check that the recipe
+  // start comes after.
+  if (recipeStart && recipeEnd) {
+    return readX(recipeStart) > readX(recipeEnd);
+  }
+  // Otherwise, check if we have no recipe end. If no recipe end, then recipe
+  // is in progress.
+  else {
+    return recipeStart && !recipeEnd;
+  }
+}
+
+export const findRunningRecipe = data => {
+  const recipeStart = findRight(data, isRecipeStart);
+  const recipeEnd = findRight(data, isRecipeEnd);
+
+  return (
+    isRecipeRunning(recipeStart, recipeEnd) ?
+    recipeStart :
+    null
+  );
+}
+
+export const readX = dataPoint =>
+  // Timestamp is in seconds. For x position, read timestamp as ms.
+  Math.round(dataPoint.timestamp * 1000);
+
+export const readY = dataPoint =>
+  Number.parseFloat(dataPoint.value);
 
 // @TODO readDataPoint
 // reformat openag-config.json to have a hashmap and a weight field, instead
