4262e95a1e3b61b362c01e2c9e82417c58666eea
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-09-25 14:28:17 -0700

    Fake toggle switches due to bug with for attr

--- scripts/common/toggle.js
+++ scripts/common/toggle.js
@@ -2,6 +2,7 @@
 A thumb toggle-style control for forms.
 */
 import {html, forward, Effects, thunk} from 'reflex';
+import {classed} from '../common/attr';
 import {update as updateUnknown} from '../common/unknown';
 
 // Actions
@@ -37,24 +38,19 @@ const change = (model, isActivated) => [
 
 // View
 
-export const view = (model, address, className) =>
+export const view = (model, address) =>
+  // @TODO there is a bug with rendering the "for" attribute on labels that
+  // is forcing us to fake checkboxes this way. In future, we should figure out
+  // if the bug is in Reflex or virtual-dom, fix and implement this with
+  // input[type=checkbox].
   html.div({
     id: model.id,
-    className
-  }, [
-    html.input({
-      type: 'checkbox',
-      checked: (
-        model.isActivated ?
-        'checked' :
-        // void(0) ensures that the property is never added to the object.
-        void(0)
-      ),
-      onChange: event => address(decodeChange(event))
+    className: classed({
+      'toggle': true,
+      'toggle--checked': model.isActivated
     }),
-    html.label({
-      'for': model.id
-    })
-  ]);
-
-const decodeChange = event => Change(event.target.checked);
+    onClick: event => {
+      event.preventDefault();
+      address(Change(!model.isActivated));
+    }
+  });
