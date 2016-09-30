478f3a994d557abbc0fa73f29842180e7d55bf8e
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-09-30 15:26:57 -0700

    Support touch events for recipe start

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,13 +1,22 @@
 import {html} from 'reflex';
+import {port} from './common/prelude';
 
 export const Activate = {
   type: 'Activate'
 };
 
-export const view = (model, address) =>
-  html.div({
+export const view = (model, address) => {
+  const sendPoint = onPoint(address);
+  return html.div({
     className: 'recipe',
-    onClick: () => address(Activate)
+    onTouchStart: sendPoint,
+    onMouseDown: sendPoint
   }, [
     String(model._id)
   ]);
+}
+
+const onPoint = port(event => {
+  event.preventDefault();
+  return Activate;
+});
