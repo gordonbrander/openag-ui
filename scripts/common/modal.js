e6fee007f5b162e25ebd2b6b1c72ab8e80db5a36
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-09-30 14:46:21 -0700

    Export event ports for modal

--- scripts/common/modal.js
+++ scripts/common/modal.js
@@ -1,6 +1,6 @@
 // Helper functions for showing and hiding model components.
 import {Effects} from 'reflex';
-import {merge} from '../common/prelude';
+import {merge, port} from '../common/prelude';
 import * as Unknown from '../common/unknown';
 
 export const Open = {
@@ -23,3 +23,13 @@ export const update = (model, action) =>
   action.type === 'Close' ?
   [merge(model, {isOpen: false}), Effects.none] :
   Unknown.update(model, action);
+
+export const onOpen = port(event => {
+  event.preventDefault();
+  return Open;
+});
+
+export const onClose = port(event => {
+  event.preventDefault();
+  return Close;
+})
