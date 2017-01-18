b2b0a18e1c6442a30d9e1e46969539d2e008a128
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-01-17 16:21:50 -0800

    Create deploy scripts for couchapp

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,19 @@
 module.exports = function (grunt) {
+  // Configuration for the CouchApp grunt plugin.
+  // We set this configuration object on properties of `grunt.initConfig`
+  // below.
+  // https://www.npmjs.com/package/grunt-couchapp.
+  var couch = {
+    openag_ui: {
+      db: 'http://localhost:5984/openag',
+      app: './couchapp.js',
+      options: {
+        okay_if_missing: true,
+        okay_if_exists: true
+      }
+    }
+  }
+
   grunt.initConfig({
     browserify: {
       dev: {
@@ -68,14 +83,20 @@ module.exports = function (grunt) {
         ],
         tasks: ["browserify:dev", "copy"]
       }
-    }
+    },
+    mkcouchdb: couch,
+    rmcouchdb: couch,
+    couchapp: couch
   });
 
   grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-couchapp");
 
   grunt.registerTask("default", ["build"]);
   grunt.registerTask("build", ["browserify:dist", "copy"]);
+  grunt.registerTask('couchapp_install', ['rmcouchdb:openag_ui', 'mkcouchdb:openag_ui', 'couchapp:openag_ui']);
+  grunt.registerTask('couchapp_deploy', ['build', 'couchapp_install']);
   grunt.registerTask("develop", ["browserify:dev", "copy", "watch"]);
 };
